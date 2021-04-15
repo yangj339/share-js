7b475553175eb82816a7740afa593f81a0b98b61
yangj339 <yangj339@gmail.com>
2021-04-15 21:26:01 +0800

    增加router

--- src/main.js
+++ src/main.js
@@ -2,10 +2,15 @@ import Vue from 'vue';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue';
+import router from './router'
 
 Vue.use(ElementUI);
-
+// 安装路由
+Vue.use(router);
 new Vue({
   el: '#app',
+  // 启用路由
+  router,
+  // 启用 ElementUI
   render: h => h(App)
 });
